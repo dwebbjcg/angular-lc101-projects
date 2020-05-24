852a78272f2ceb57b80498c629b245a0c55b11ac
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-05-24 14:38:14 -0400

    First commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Life';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://image.freepik.com/free-vector/music-colorful-illustration_24908-57101.jpg';
   image3 = '';
 
   constructor() { }
