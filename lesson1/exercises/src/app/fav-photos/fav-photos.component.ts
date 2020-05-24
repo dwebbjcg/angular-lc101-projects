0e14a7c2f3349dcaf36d22c579d03b719e808dbc
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-05-24 15:44:43 -0400

    Second commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -9,7 +9,7 @@ export class FavPhotosComponent implements OnInit {
   photosTitle = 'My Life';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'https://image.freepik.com/free-vector/music-colorful-illustration_24908-57101.jpg';
-  image3 = '';
+  image3 = 'https://www.mcas.k12.in.us/cms/lib/IN01001792/Centricity/Domain/1202/Coding%20GIF%206%2011-12-2019.gif';
 
   constructor() { }
 
